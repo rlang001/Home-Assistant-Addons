712711ab078429b8da142fc95f2b926865087879
rlang001 <richalang@gmail.com>
2023-12-29 14:46:03 -0500

    modified:   OpenWebRX/config.yaml new file:   OpenWebRX/rootfs/configs/users.json modified:   OpenWebRX/run.sh

--- OpenWebRX/run.sh
+++ OpenWebRX/run.sh
@@ -7,20 +7,36 @@ if [ -z "$(ls -A /config/openwebrx 2>/dev/null)" ]; then
     bashio::log.info "Creating default OpenWebRX configuration under /config/openwebrx"
     mkdir -p /config/openwebrx
 
-    bashio::log.info "Update the /config/openwebrx/weewx.conf and restart"
     # Copy the openwebrx.conf
     cp -u /configs/openwebrx.conf /config/openwebrx/openwebrx.conf
-    # Remove the original
-    rm -f /etc/openwebrx/openwebrx.conf
-    # Link to ours
-    ln -s /config/openwebrx/openwebrx.conf /etc/openwebrx/openwebrx.conf 
-
     # Copy the settings file if it doesn't exist or is newer
     cp -u /configs/settings.json /config/openwebrx/settings.json
     cp -u /configs/bookmarks.json /config/openwebrx/bookmarks.json
     cp -u /configs/users.json /config/openwebrx/users.json
-    exit 0 
-fi   
+
+    bashio::log.info "Update the /config/openwebrx/weewx.conf and restart"
+    exit 0
+fi
+
+# Remove the original
+rm -f /etc/openwebrx/openwebrx.conf
+# Link to ours
+ln -s /config/openwebrx/openwebrx.conf /etc/openwebrx/openwebrx.conf 
+
+if [ -z "$(ls -A /config/openwebrx/settings.json 2>/dev/null)" ]; then
+    # Copy the settings file if it doesn't exist or is newer
+    cp -u /configs/settings.json /config/openwebrx/settings.json
+fi
+
+if [ -z "$(ls -A /config/openwebrx/bookmarks.json 2>/dev/null)" ]; then
+    cp -u /configs/bookmarks.json /config/openwebrx/bookmarks.json
+fi
+
+if [ -z "$(ls -A /config/openwebrx/bookmarks.json 2>/dev/null)" ]; then
+    cp -u /configs/users.json /config/openwebrx/users.json
+fi
+
+
 
 # Remove the default settings file
 rm /var/lib/openwebrx/settings.json
