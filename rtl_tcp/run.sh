51345d5168abe80cd9adb25b0215d2b97290c327
rlang001 <richalang@gmail.com>
2023-12-29 11:21:26 -0500

    modified:   rtl_tcp/config.yaml modified:   rtl_tcp/run.sh

--- rtl_tcp/run.sh
+++ rtl_tcp/run.sh
@@ -2,5 +2,18 @@
 export PATH="$PATH:/usr/local/bin"
 CONFIG_PATH=/data/options.json
 
+IP="$(hostname -I)"
+DEVICE="$(bashio::config 'device')"
+FREQUENCY="$(bashio::config 'frequency')"
+GAIN="$(bashio::config 'gain')"
+SAMPLERATE="$(bashio::config 'samplerate')"
+BUFFERS="$(bashio::config 'buffers')"
+
+exec rtl_tcp -a ${IP} 
+    -d ${DEVICE} \
+    -f ${FREQUENCY}
+    -g ${GAIN} \
+    -s ${SAMPLERATE} \
+    -b ${BUFFERS}
 
 sleep infinity
