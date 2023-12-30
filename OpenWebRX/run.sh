645e020407ee57516afb6d8d54ac23209e4c490f
rlang001 <richalang@gmail.com>
2023-12-30 18:16:28 -0500

    modified:   OpenWebRX/config.yaml modified:   OpenWebRX/rootfs/root/etc/openwebrx/openwebrx.conf modified:   OpenWebRX/run.sh

--- OpenWebRX/run.sh
+++ OpenWebRX/run.sh
@@ -28,20 +28,22 @@ if [ -z "$(ls -A /config/openwebrx/bookmarks.json 2>/dev/null)" ]; then
     cp -u /configs/bookmarks.json /config/openwebrx/bookmarks.json
 fi
 
-if [ -z "$(ls -A /config/openwebrx/users.json 2>/dev/null)" ]; then
+# Always copy a new users file
+#if [ -z "$(ls -A /config/openwebrx/users.json 2>/dev/null)" ]; then
     cp -u /var/lib/openwebrx/users.json /config/openwebrx/users.json
-fi
+#fi
 
 
 # Remove the default settings file
 rm /var/lib/openwebrx/settings.json
 rm /var/lib/openwebrx/bookmarks.json
-rm /var/lib/openwebrx/users.json
+#rm /var/lib/openwebrx/users.json
 
 # Point it to ours
 ln -s /config/openwebrx/settings.json /var/lib/openwebrx/settings.json
 ln -s /config/openwebrx/bookmarks.json /var/lib/openwebrx/bookmarks.json
-ln -s /config/openwebrx/users.json /var/lib/openwebrx/users.json
+
+#ln -s /config/openwebrx/users.json /var/lib/openwebrx/users.json
 
 
 # Set the user/password.  This modifies the /var/lib/openwebrx/users.json file
