30d884299d9e04fb47b963080dcc2d1d4bf4f769
rlang001 <richalang@gmail.com>
2023-12-30 11:06:05 -0500

    modified:   OpenWebRX/config.yaml modified:   OpenWebRX/run.sh

--- OpenWebRX/run.sh
+++ OpenWebRX/run.sh
@@ -11,13 +11,14 @@ if [ -z "$(ls -A /config/openwebrx 2>/dev/null)" ]; then
     mkdir -p /config/openwebrx
 
     # Copy the openwebrx.conf
-#    cp -u /configs/openwebrx.conf /config/openwebrx/openwebrx.conf
+    # cp -u /configs/openwebrx.conf /config/openwebrx/openwebrx.conf
+
     # Copy the settings file if it doesn't exist or is newer
     cp -u /configs/settings.json /config/openwebrx/settings.json
     cp -u /configs/bookmarks.json /config/openwebrx/bookmarks.json
     cp -u /configs/users.json /config/openwebrx/users.json
 
-    bashio::log.info "Update the /config/openwebrx/weewx.conf and restart"
+    bashio::log.info "Please restart"
     exit 0
 fi
 
@@ -26,33 +27,33 @@ fi
 # Link to ours
 #ln -s /config/openwebrx/openwebrx.conf /etc/openwebrx/openwebrx.conf 
 
-#if [ -z "$(ls -A /config/openwebrx/settings.json 2>/dev/null)" ]; then
+if [ -z "$(ls -A /config/openwebrx/settings.json 2>/dev/null)" ]; then
     # Copy the settings file if it doesn't exist or is newer
-#    cp -u /configs/settings.json /config/openwebrx/settings.json
-#fi
+    cp -u /configs/settings.json /config/openwebrx/settings.json
+fi
 
-#if [ -z "$(ls -A /config/openwebrx/bookmarks.json 2>/dev/null)" ]; then
-#    cp -u /configs/bookmarks.json /config/openwebrx/bookmarks.json
-#fi
+if [ -z "$(ls -A /config/openwebrx/bookmarks.json 2>/dev/null)" ]; then
+    cp -u /configs/bookmarks.json /config/openwebrx/bookmarks.json
+fi
 
 # Set the user/password
 /root/useradmin.exp ${USER} ${PASSWORD}
 
 
-#if [ -z "$(ls -A /config/openwebrx/users.json 2>/dev/null)" ]; then
-#    cp -u /var/lib/openwebrx/users.json /config/openwebrx/users.json
-#fi
+if [ -z "$(ls -A /config/openwebrx/users.json 2>/dev/null)" ]; then
+    cp -u /var/lib/openwebrx/users.json /config/openwebrx/users.json
+fi
 
 
 # Remove the default settings file
-#rm /var/lib/openwebrx/settings.json
-#rm /var/lib/openwebrx/bookmarks.json
-#rm /var/lib/openwebrx/users.json
+rm /var/lib/openwebrx/settings.json
+rm /var/lib/openwebrx/bookmarks.json
+rm /var/lib/openwebrx/users.json
 
 # Point it to ours
-#ln -s /config/openwebrx/settings.json /var/lib/openwebrx/settings.json
-#ln -s /config/openwebrx/bookmarks.json /var/lib/openwebrx/bookmarks.json
-#ln -s /config/openwebrx/users.json /var/lib/openwebrx/users.json
+ln -s /config/openwebrx/settings.json /var/lib/openwebrx/settings.json
+ln -s /config/openwebrx/bookmarks.json /var/lib/openwebrx/bookmarks.json
+ln -s /config/openwebrx/users.json /var/lib/openwebrx/users.json
 
 # Start openwebrx
 openwebrx
