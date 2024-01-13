800518eabd51bddcb983bded3d5f2c3d26a59606
rlang001 <richalang@gmail.com>
2024-01-13 17:28:21 -0500

    modified:   shinySDR/Dockerfile new file:   shinySDR/rootfs/etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf deleted:    shinySDR/rootfs/etc/nginx/includes/fastcgi_params.conf deleted:    shinySDR/rootfs/etc/nginx/includes/mime.types deleted:    shinySDR/rootfs/etc/nginx/includes/server_params.conf deleted:    shinySDR/rootfs/etc/nginx/includes/ssl_params.conf deleted:    shinySDR/rootfs/etc/nginx/nginx.conf deleted:    shinySDR/rootfs/etc/nginx/servers/.gitkeep deleted:    shinySDR/rootfs/etc/nginx/templates/direct.gtpl deleted:    shinySDR/rootfs/etc/nginx/templates/ingress.gtpl deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/.init-weewx/dependencies.d/base deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/.init-weewx/run deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/.init-weewx/type deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/.init-weewx/up deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/.weewx/dependencies.d/init-weewx deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/.weewx/finish deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/.weewx/run deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/.weewx/type deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/init-nginx/dependencies.d/base deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/init-nginx/run deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/init-nginx/type deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/init-nginx/up deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/nginx/dependencies.d/init-nginx deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/nginx/finish deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/nginx/run deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/nginx/type deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/user/contents.d/.init-weewx deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/user/contents.d/.weewx deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/user/contents.d/init-nginx deleted:    shinySDR/rootfs/etc/s6-overlay/s6-rc.d/user/contents.d/nginx new file:   shinySDR/rootfs/etc/udev/rules.d/rtl-sdr.rules new file:   shinySDR/rootfs/root/.bashrc new file:   shinySDR/rootfs/root/.profile modified:   shinySDR/run.sh

--- shinySDR/run.sh
+++ shinySDR/run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bashio
-#export PATH="$PATH:/home/weewx/bin"
+export PATH="$PATH:/usr/local/bin"
+
+export LD_LIBRARY_PATH=/usr/local/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+
+
 CONFIG_PATH=/data/options.json
 
-sleep infinity
 
+sleep infinity
