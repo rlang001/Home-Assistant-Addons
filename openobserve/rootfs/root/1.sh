09e57c1b30f9e361c207592e568c6fa20cb7b273
Richard Lang <50710759+rlang001@users.noreply.github.com>
2026-01-21 16:53:50 -0500

    Update 1.sh

--- openobserve/rootfs/root/1.sh
+++ openobserve/rootfs/root/1.sh
@@ -1,1 +1,14 @@
 
+#!/command/with-contenv bashio
+# shellcheck shell=bash
+# ==============================================================================
+# Home Assistant Community Add-on: 
+# Configures rsyslog
+# ==============================================================================
+set -e
+
+declare -A mylevel
+
+CONFIG_PATH=/data/options.json
+
+jq '. + {"hostname": `hostname -I`}' /data/options.json > /data/options.json.tmpi
