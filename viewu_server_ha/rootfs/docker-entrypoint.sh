dff94a904e6bb14e3e281178ffeff1d415b40527
Richard Lang <50710759+rlang001@users.noreply.github.com>
2026-06-05 10:49:57 -0400

    Update docker-entrypoint.sh

--- viewu_server_ha/rootfs/docker-entrypoint.sh
+++ viewu_server_ha/rootfs/docker-entrypoint.sh
@@ -37,10 +37,10 @@ fi
 bashio::log.info "Starting Viewu Server..."
 cd /usr/local/bin/
 
-export MQTT_SERVER="192.168.10.9"
-export MQTT_PORT="1883"
+#export MQTT_SERVER="192.168.10.9"
+#export MQTT_PORT="1883"
 #export MQTT_USER="mqtt"
-export MQTT_PASSWORD="Sadi3Bail33"
+#export MQTT_PASSWORD="Sadi3Bail33"
 
-#exec sh -c viewu_server $MQTT_SERVER $MQTT_PORT $MQTT_USER $MQTT_PASSWORD
+exec sh -c viewu_server $MQTT_SERVER $MQTT_PORT $MQTT_USER $MQTT_PASSWORD
 #exec sh -c viewu_server 192.168.10.9 1883 mqtt Sadi3Bail33
